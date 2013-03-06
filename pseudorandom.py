73f75efcfe69210d8e22ff55c19b02b7408b9671
Trey Hunner <trey@treyhunner.com>
2013-03-06 11:06:25 -0800

    Use environment variable PORT for flask port

--- pseudorandom.py
+++ pseudorandom.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 from names import get_full_name
 
@@ -11,4 +12,5 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
