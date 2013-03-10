0ef630bfee5a57ba2e5d487707249cdaa43ec63f
Trey Hunner <trey@treyhunner.com>
2013-03-10 15:40:50 -0700

    Send text response when Content-Type is text/plain

--- pseudorandom.py
+++ pseudorandom.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
 from names import get_full_name
 
 
@@ -9,8 +9,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    if request.headers.get('User-Agent', '')[:4].lower() == 'curl':
-        return u"{0}\n".format(get_full_name())
+    if (request.headers.get('User-Agent', '')[:4].lower() == 'curl' or
+        request.headers['Content-Type'] == 'text/plain'):
+        return make_response((u"{0}\n".format(get_full_name()), 200,
+                              {'Content-Type': 'text/plain'}))
     else:
         return render_template('index.html', name=get_full_name())
 
